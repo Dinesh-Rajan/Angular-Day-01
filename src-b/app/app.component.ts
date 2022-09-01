e049e6cc8655164f605f9003d93004a079d598e5
Dinesh-Rajan <110998797+Dinesh-Rajan@users.noreply.github.com>
2022-09-01 14:46:43 +0530

    Add files via upload

---
+++ src-b/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name:string = 'Dinesh Adili Rajan';
+  address = 'Samatha Nagar, Matunga Labour Camp, Mumbai-400019';
+  contact = 'adilidinesh@gmail.com';
+}
